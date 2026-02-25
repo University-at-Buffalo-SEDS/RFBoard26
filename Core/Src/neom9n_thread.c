e8ff2494dd38f72f912fdfea33377be95bdf5c59
Rylan Meilutis <rylan.meilutis@gmail.com>
2026-02-25 00:29:54 -0500

    uart 1 is the only available one

--- Core/Src/neom9n_thread.c
+++ Core/Src/neom9n_thread.c
@@ -68,7 +68,7 @@
 
 // External SPI handle
 extern SPI_HandleTypeDef hspi1;
-extern UART_HandleTypeDef huart2;
+extern UART_HandleTypeDef huart1;
 
 #ifndef TX_TIMER_TICKS_PER_SECOND
 #error "TX_TIMER_TICKS_PER_SECOND must be defined by ThreadX."
@@ -119,7 +119,7 @@ void neom9n_thread_entry(ULONG initial_input)
 
     // Configure for rocket flight
     // @note: Confirm with datasheet, after first config this block is likely reducdent
-    if (config_gps_seds_rocket(&hspi1, &huart2, 5000))
+    if (config_gps_seds_rocket(&hspi1, &huart1, 5000))
     {
         // const char success[] = "NEOM9N config set successful";
         // log_telemetry_asynchronous(SEDS_DT_MESSAGE_DATA,
