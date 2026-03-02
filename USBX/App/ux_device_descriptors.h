0ba992e04d9ef1e65961be38ca574d9d75f5d85b
Rylan Meilutis <rylan.meilutis@gmail.com>
2026-03-02 16:59:12 -0500

    new firmware version and bette naming

--- USBX/App/ux_device_descriptors.h
+++ USBX/App/ux_device_descriptors.h
@@ -269,9 +269,9 @@ uint16_t USBD_Get_Configuration_Number(uint8_t class_type, uint8_t interface_typ
 #define USBD_VID                                      1155
 #define USBD_PID                                      22288
 #define USBD_LANGID_STRING                            1033
-#define USBD_MANUFACTURER_STRING                      "STMicroelectronics"
-#define USBD_PRODUCT_STRING                           "STM32 USB Device"
-#define USBD_SERIAL_NUMBER                            "000000000001"
+#define USBD_MANUFACTURER_STRING                      "UBSEDS"
+#define USBD_PRODUCT_STRING                           "CHUD RF Board"
+#define USBD_SERIAL_NUMBER                            "67"
 
 #define USB_DESC_TYPE_INTERFACE                       0x04U
 #define USB_DESC_TYPE_ENDPOINT                        0x05U
