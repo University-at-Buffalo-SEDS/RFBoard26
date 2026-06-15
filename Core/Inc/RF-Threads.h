4e564948e18b5cc65d09199b628b2d090f1c609b
Rylan Meilutis <rylan.meilutis@gmail.com>
2026-06-15 18:43:20 -0500

    better gps reporting

--- Core/Inc/RF-Threads.h
+++ Core/Inc/RF-Threads.h
@@ -13,8 +13,6 @@ UINT create_telemetry_thread(TX_BYTE_POOL *byte_pool);
 /* ------- NEOM9N Thread -------- */
 extern TX_THREAD neom9n_thread;
 extern volatile uint8_t g_neom9n_has_fix;
-extern volatile uint8_t g_neom9n_satellite_count;
-extern volatile uint32_t g_neom9n_satellite_update_tick;
 
 void neom9n_thread_entry(ULONG initial_input);
 UINT create_neom9n_thread(TX_BYTE_POOL *byte_pool);
